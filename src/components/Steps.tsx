bf765258d242d7d32a607ca0885fd4ec48395c72
Derek Prieur <DPrieur123@gmail.com>
2024-05-07 09:52:40 -0400

    update

--- src/components/Steps.tsx
+++ src/components/Steps.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { cn } from "@/lib/utils";
 import { usePathname } from "next/navigation";
 
 const STEPS = [
@@ -29,7 +30,68 @@ const Steps = () => {
         const isCompleted = STEPS.slice(index + 1).some((s) =>
           pathname.endsWith(s.url),
         );
-        return <li key={index}></li>;
+        const imgPath = `/snake-${index + 1}.png`;
+        return (
+          <li key={step.name} className="relative overflow-hidden lg:flex-1">
+            <div>
+              <span
+                className={cn(
+                  "absolute left-0 top-0 h-full w-1 bg-zinc-400 lg:bottom-0 lg:top-auto lg:h-1 lg:w-full",
+                  isCurrent && "bg-zinc-700",
+                  isCompleted && "bg-primary",
+                )}
+                aria-hidden="true"
+              />
+              <span
+                className={cn(
+                  index !== 0 && "lg:pl-9",
+                  "flex items-center px-6 py-4 text-sm font-medium",
+                )}
+              >
+                <span className="flex-shrink-0">
+                  <img
+                    src={imgPath}
+                    className={cn(
+                      "flex h-20 w-20 items-center justify-center object-contain",
+                      isCompleted && "border-none",
+                      isCurrent && "border-zinc-700",
+                    )}
+                  />
+                </span>
+                <span className="ml-4 mt-0.5 flex h-full min-w-0 flex-col justify-center">
+                  <span
+                    className={cn(
+                      "text-sm font-semibold text-zinc-700",
+                      isCompleted && "text-primary",
+                      isCurrent && "text-zinc-700",
+                    )}
+                  >
+                    {step.name}
+                  </span>
+                  <span className="text-sm text-zinc-500">
+                    {step.description}
+                  </span>
+                </span>
+              </span>
+              {index !== 0 && (
+                <div className="absolute inset-0 hidden w-3 lg:block">
+                  <svg
+                    className="h-full w-full text-gray-300"
+                    viewBox="0 0 12 82"
+                    fill="none"
+                    preserveAspectRatio="none"
+                  >
+                    <path
+                      d="M0.5 0V31L10.5 41L0.5 51V82"
+                      stroke="currentcolor"
+                      vectorEffect="non-scaling-stroke"
+                    />
+                  </svg>
+                </div>
+              )}
+            </div>
+          </li>
+        );
       })}
     </ol>
   );
