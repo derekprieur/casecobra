e578bb6f99144f68f2f1321ad283b35202803d29
Derek Prieur <DPrieur123@gmail.com>
2024-05-07 13:45:13 -0400

    update

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -1,8 +1,13 @@
 "use client";
 
+import HandleComponent from "@/components/HandleComponent";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { cn } from "@/lib/utils";
 import NextImage from "next/image";
+import { Rnd } from "react-rnd";
+import { RadioGroup } from "@headlessui/react";
+import { useState } from "react";
 
 interface DesignConfiguratorProps {
   configId: string;
@@ -18,6 +23,8 @@ const DesignConfigurator = ({
   imageUrl,
   imageDimensions,
 }: DesignConfiguratorProps) => {
+  const [options, setOptions] = useState();
+
   return (
     <div className="relative mb-20 mt-20 grid grid-cols-3 pb-20">
       <div className="relative col-span-2 flex h-[37.5rem] w-full max-w-4xl items-center justify-center overflow-hidden rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2">
@@ -41,14 +48,48 @@ const DesignConfigurator = ({
             )}
           />
         </div>
-        <div className="relative h-full w-full">
-          <NextImage
-            src={imageUrl}
-            fill
-            alt="your image"
-            className="pointer-events-none"
+        <Rnd
+          default={{
+            x: 150,
+            y: 205,
+            height: imageDimensions.height / 4,
+            width: imageDimensions.width / 4,
+          }}
+          lockAspectRatio
+          className="absolute z-20 border-[3px] border-primary"
+          resizeHandleComponent={{
+            bottomRight: <HandleComponent />,
+            bottomLeft: <HandleComponent />,
+            topLeft: <HandleComponent />,
+            topRight: <HandleComponent />,
+          }}
+        >
+          <div className="relative h-full w-full">
+            <NextImage
+              src={imageUrl}
+              fill
+              alt="your image"
+              className="pointer-events-none"
+            />
+          </div>
+        </Rnd>
+      </div>
+      <div className="flex h-[37.5rem] flex-col bg-white">
+        <ScrollArea className="relative flex-1 overflow-auto">
+          <div
+            aria-hidden="true"
+            className="pointer-events-none absolute inset-x-0 bottom-0 z-10 h-12 bg-gradient-to-t from-white"
           />
-        </div>
+          <div className="px-8 pb-12 pt-8">
+            <h2 className="text-3xl font-bold tracking-tight">
+              Customize your case
+            </h2>
+            <div className="my-6 h-px w-full bg-zinc-200" />
+            <div className="relative mt-4 flex h-full flex-col justify-between">
+              <RadioGroup value=""></RadioGroup>
+            </div>
+          </div>
+        </ScrollArea>
       </div>
     </div>
   );
