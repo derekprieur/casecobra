dcdba754feba3ba6e9923709f03a63eb6e64b998
Derek Prieur <DPrieur123@gmail.com>
2024-05-09 09:53:40 -0400

    update

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,113 @@
-const DashboardPage = () => {
-  return <div className="flex min-h-screen w-full bg-muted/40"></div>;
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Progress } from "@/components/ui/progress";
+import { db } from "@/db";
+import { formatPrice } from "@/lib/utils";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { notFound } from "next/navigation";
+
+const WEEKLY_GOAL = 500;
+const MONTHLY_GOAL = 2500;
+
+const DashboardPage = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  const adminEmail = process.env.ADMIN_EMAIL;
+  const orders = await db.order.findMany({
+    where: {
+      isPaid: true,
+      createdAt: {
+        gte: new Date(new Date().setDate(new Date().getDate() - 7)),
+      },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+    include: {
+      user: true,
+      shippingAddress: true,
+    },
+  });
+  const lastWeekSum = await db.order.aggregate({
+    where: {
+      isPaid: true,
+      createdAt: {
+        gte: new Date(new Date().setDate(new Date().getDate() - 7)),
+      },
+    },
+    _sum: {
+      amount: true,
+    },
+  });
+  const lastMonthSum = await db.order.aggregate({
+    where: {
+      isPaid: true,
+      createdAt: {
+        gte: new Date(new Date().setDate(new Date().getDate() - 30)),
+      },
+    },
+    _sum: {
+      amount: true,
+    },
+  });
+
+  if (!user || user.email !== adminEmail) {
+    return notFound();
+  }
+
+  return (
+    <div className="flex min-h-screen w-full bg-muted/40">
+      <div className="mx-auto flex w-full max-w-7xl flex-col sm:gap-4 sm:py-4">
+        <div className="flex flex-col gap-16">
+          <div className="grid gap-4 sm:grid-cols-2">
+            <Card>
+              <CardHeader className="pb-2">
+                <CardDescription>Last Week</CardDescription>
+                <CardTitle className="text-4xl">
+                  {formatPrice(lastWeekSum._sum.amount ?? 0)}
+                </CardTitle>
+              </CardHeader>
+              <CardContent>
+                <div className="text-sm text-muted-foreground">
+                  of {formatPrice(WEEKLY_GOAL)} goal
+                </div>
+              </CardContent>
+              <CardFooter>
+                <Progress
+                  value={((lastWeekSum._sum.amount ?? 0) / WEEKLY_GOAL) * 100}
+                />
+              </CardFooter>
+            </Card>
+            <Card>
+              <CardHeader className="pb-2">
+                <CardDescription>Last Month</CardDescription>
+                <CardTitle className="text-4xl">
+                  {formatPrice(lastMonthSum._sum.amount ?? 0)}
+                </CardTitle>
+              </CardHeader>
+              <CardContent>
+                <div className="text-sm text-muted-foreground">
+                  of {formatPrice(MONTHLY_GOAL)} goal
+                </div>
+              </CardContent>
+              <CardFooter>
+                <Progress
+                  value={((lastMonthSum._sum.amount ?? 0) / MONTHLY_GOAL) * 100}
+                />
+              </CardFooter>
+            </Card>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardPage;
